20ea78be42667587fef35fc5433789f52613b094
Ines Radic <inesradic12314@gmail.com>
2021-02-02 12:21:29 -0800

    add express app and parse json

---
+++ server.js
@@ -0,0 +1,22 @@
+// add express server
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// include midleware to parse json
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// html routes (routes that will serve websites)
+//define landing page route 
+  // use index.html inside public folder
+// note page route 
+  // use notes.html inside public folder
+//API routes 
+// post route to save a note 
+  // ADD the new note to the db.json
+// get route to get all notes
+// put route to update a note (app.put())
+// delete route to delete a note
+// invoke listen function on express app
