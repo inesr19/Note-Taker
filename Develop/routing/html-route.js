772276b98395638fe08f4c5357a8b0d23e8bd622
Ines Radic <inesradic12314@gmail.com>
2021-02-02 18:12:09 -0800

    add route to html

---
+++ Develop/routing/html-route.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = function (app) {
+    app.get('/', function(req, res){
+        res.sendFile(path.join(__dirname + '/../Public/index.html'));
+    });
+
+// note page route 
+// use notes.html inside public folder
+    app.get('/notes', function(req, res){
+      res.sendFile(path.join(__dirname + '/../Public/notes.html'));
+  });
+}
--- server.js
+++ server.js
@@ -14,16 +14,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
-  })
 // html routes (routes that will serve websites)
 //define landing page route 
   // use index.html inside public folder
-// note page route 
-  // use notes.html inside public folder
+  
+
+  require('./Develop/routing/html-route')(app);
 //API routes 
 // post route to save a note 
   // ADD the new note to the db.json
