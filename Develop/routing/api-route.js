9b2d4c94f5f5423c7e40d78b37432eba6cb43671
Ines Radic <inesradic12314@gmail.com>
2021-02-03 23:03:30 -0800

    add post route

--- Develop/routing/api-route.js
+++ Develop/routing/api-route.js
@@ -6,4 +6,12 @@ module.exports = function (app) {
     app.get('/api/notes', function(req, res) {
         res.json(data);
     });
+
+    app.post('/api/notes', function (req, res) {
+        const notes = req.body;
+        data.push(notes.title, notes.text);
+        res.status(201);
+        res.end();
+
+    })
 }
